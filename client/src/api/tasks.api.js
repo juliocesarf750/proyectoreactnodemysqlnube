42d09403274488e5a46b2bc01225b2386125b625
julio cesar fuentes <juliocesarfuentesfuentes16@gmail.com>
2023-07-20 17:11:16 -0400

    cuarto commit puerto 3000 arrglado y base mysql en la nube

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,20 +1,20 @@
 import axios from 'axios';
 
 export const getTaskRequest = async() =>
-        await axios.get('http://localhost:4000/tasks');
+        await axios.get('http://localhost:3000/tasks');
 
 
 export const createTasksRequest = async(task) =>
-        await axios.post('http://localhost:4000/tasks',task);
+        await axios.post('http://localhost:3000/tasks',task);
 
 export const deleteTaskRequest = async (id) =>
-       await axios.delete(`http://localhost:4000/tasks/${id}`);
+       await axios.delete(`http://localhost:3000/tasks/${id}`);
 
 export const getTasksRequest = async(id)=>
-     await axios.get(`http://localhost:4000/tasks/${id}`);
+     await axios.get(`http://localhost:3000/tasks/${id}`);
 
 export const updateTaskRequest = async (task,id) =>
-       await axios.put(`http://localhost:4000/tasks/${id}`,task);
+       await axios.put(`http://localhost:3000/tasks/${id}`,task);
 
 export const toggleTaskDoneRequest = async(id,Done)=>
-      await axios.put(`http://localhost:4000/tasks/${id}`,{ Done},);
+      await axios.put(`http://localhost:3000/tasks/${id}`,{ Done},);
--- server/config.js
+++ server/config.js
@@ -1,6 +1,6 @@
-export const PORT = process.env.PORT || 4000;
-export const DB_HOST = process.env.DB_HOST || 'containers-us-west-84.railway.app';
+export const PORT = process.env.PORT || 3000;
+export const DB_HOST = process.env.DB_HOST || 'containers-us-west-112.railway.app';
 export const DB_USER = process.env.DB_USER || 'root';
-export const DB_PASSWORD = process.env.DB_PASSWORD || 'aLvRaGAuotWos7neOci6';
-export const DB_NAME = process.env.DB_NAME || 'trailway';
-export const DB_PORT = process.env.DB_PORT || 7998 ;
+export const DB_PASSWORD = process.env.DB_PASSWORD || 'MsDMA89tfMYL5n7DMDio';
+export const DB_NAME = process.env.DB_NAME || 'railway';
+export const DB_PORT = process.env.DB_PORT || 6495 ;
