a6c7bf50333e91bef15bd26f468e1f1157f42399
julio cesar fuentes <juliocesarfuentesfuentes16@gmail.com>
2023-07-21 20:41:08 -0400

    sexto commit base arreglada en  base mysql en la nube

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,20 +1,28 @@
 import axios from 'axios';
 
 export const getTaskRequest = async() =>
+
         await axios.get('https://wild-sun-3186.fly.dev/tasks');
+        //await axios.get('http://localhost:3000/tasks');
 
 
 export const createTasksRequest = async(task) =>
         await axios.post('https://wild-sun-3186.fly.dev/tasks',task);
+        //await axios.post('http://localhost:3000/tasks',task);
 
 export const deleteTaskRequest = async (id) =>
        await axios.delete(`https://wild-sun-3186.fly.dev/tasks/${id}`);
+       //await axios.delete(`http://localhost:3000/tasks/${id}`);
 
 export const getTasksRequest = async(id)=>
      await axios.get(`https://wild-sun-3186.fly.dev/tasks/${id}`);
+     //await axios.get(`http://localhost:3000/tasks/${id}`);
+
 
 export const updateTaskRequest = async (task,id) =>
        await axios.put(`https://wild-sun-3186.fly.dev/tasks/${id}`,task);
+       //await axios.put(`http://localhost:3000/${id}`,task);
 
 export const toggleTaskDoneRequest = async(id,Done)=>
-      await axios.put(`https://wild-sun-3186.fly.dev/tasks/${id}`,{ Done},);
+     await axios.put(`https://wild-sun-3186.fly.dev/tasks/${id}`,{ Done},);
+     //await axios.put(`http://localhost:3000/${id}`,{ Done},);
